73558988cccac7e9059bd5038d2ac64ae0226cc3
Bishoy640 <wolverinebishoy@gmail.com>
2020-09-19 14:15:09 +0200

    up

---
+++ src/wallpaper.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './wallpaper.css';
+
+const wallpaper = () => {
+    return (
+        <div className="area">
+
+            <ul className="circles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+
+
+    )
+
+};
+
+export default wallpaper
